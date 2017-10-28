548734bdd701418ebbdb4405d004bd67dedbb5b5
Konrad Korus <konradkorus@gmail.com>
2017-10-28 12:32:17 +0100

    Added rewire package.

--- node-tests/spies/app.js
+++ node-tests/spies/app.js
@@ -1,4 +1,4 @@
-const db = require('./db');
+let db = require('./db');
 
 module.exports.handleSignup = (email, password) => {
     db.saveUser({
--- node-tests/spies/app.test.js
+++ node-tests/spies/app.test.js
@@ -1,10 +1,30 @@
 const expect = require('expect');
+const rewire = require('rewire');
+
+var app = rewire('./app');
 
 describe('App', () => {
+    var db = {
+        saveUser: expect.createSpy()
+    };
+
+    app.__set__('db', db);
+
     it('should call the spy correctly', () => {
         var spy = expect.createSpy();
         spy();
-
         expect(spy).toHaveBeenCalled();
+    });
+
+    it('should call saveUser with user object', () => {
+        // Arrange
+        var email = 'johndoe@gmail.com';
+        var password = '123abc';
+
+        // Act
+        app.handleSignup(email, password);
+
+        // Assert
+        expect(db.saveUser).toHaveBeenCalledWith({ email, password });
     });
 });
