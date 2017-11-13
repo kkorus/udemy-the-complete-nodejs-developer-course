71906ddc1bab90ee3be183ea4fe156f162d05cc9
Konrad Korus <konradkorus@gmail.com>
2017-11-13 21:51:07 +0000

    Add POST /users/login route

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -116,9 +116,21 @@ app.post('/users', (req, res) => {
         });
 });
 
-
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then(user => {
+        return user.generateAuthToken().then(token => {
+            res.header('x-auth', token).send(user);
+        });
+        res.send(user);
+    }).catch(err => {
+        res.status(400).send();
+    });
 });
 
 const port = process.env.PORT || 3000;
