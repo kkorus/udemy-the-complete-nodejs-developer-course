c2219e3d3f07d7cd6bfb0f1a58d6a1d53cd08ae6
Konrad Korus <konradkorus@gmail.com>
2017-11-16 21:28:29 +0000

    Fixed POST /users/login test

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -121,13 +121,12 @@ app.get('/users/me', authenticate, (req, res) => {
 });
 
 app.post('/users/login', (req, res) => {
-    var body = _.pick(req.body, ['email', 'password']);
+    const body = _.pick(req.body, ['email', 'password']);
 
     User.findByCredentials(body.email, body.password).then(user => {
         return user.generateAuthToken().then(token => {
             res.header('x-auth', token).send(user);
         });
-        res.send(user);
     }).catch(err => {
         res.status(400).send();
     });
--- node-todo-api/tests/server.test.js
+++ node-todo-api/tests/server.test.js
@@ -263,13 +263,13 @@ describe('POST /todos', () => {
         });
     });
 
-    describe('POST /users/login', () => {
+    describe.only('POST /users/login', () => {
         it('should login user and return auth token', (done) => {
             request(app)
                 .post('/users/login')
                 .send({
                     email: users[1].email,
-                    password: users[1].passowrd
+                    password: users[1].password
                 })
                 .expect(200)
                 .expect(res => {
@@ -282,7 +282,6 @@ describe('POST /todos', () => {
                     }
 
                     User.findById(users[1]._id).then(user => {
-                        // console.log(user);
                         expect(user.tokens[0]).toInclude({
                             access: 'auth',
                             token: res.headers['x-auth']
